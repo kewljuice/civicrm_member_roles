1e76c559914bf6bb30e252d57c2204e1550df2b0
Wouter H <wouter.hechtermans@calibrate.be>
2022-03-15 14:48:13 +0100

    Removed entity.manager
    https://api.drupal.org/api/drupal/core!lib!Drupal.php/function/Drupal%3A%3AentityManager/8.2.x

--- src/CivicrmMemberRoleRuleListBuilder.php
+++ src/CivicrmMemberRoleRuleListBuilder.php
@@ -52,7 +52,7 @@ class CivicrmMemberRoleRuleListBuilder extends ConfigEntityListBuilder {
   public static function createInstance(ContainerInterface $container, EntityTypeInterface $entity_type) {
     return new static(
       $entity_type,
-      $container->get('entity.manager')->getStorage($entity_type->id()),
+      $container->get('entity_type.manager')->getStorage($entity_type->id()),
       $container->get('entity_type.manager')->getStorage('user_role'),
       $container->get('civicrm_member_roles')
     );
